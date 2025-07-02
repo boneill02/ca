2bd626421042df9fca645d32d2df8dc775b26472
Ben O'Neill <ben@oneill.sh>
2025-07-02 11:16:54 -0400

    Format

--- ca.js
+++ ca.js
@@ -2,7 +2,7 @@
  * @file ca.js
  * @brief Cellular automata simulator
  * @author Ben O'Neill <ben@oneill.sh>
- * 
+ *
  * @copyright Copyright (c) 2022-2025 Ben O'Neill <ben@oneill.sh>.
  * This work is released under the terms of the MIT License. See
  * LICENSE.
